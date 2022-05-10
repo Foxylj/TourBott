b1bdd13235b3284316fc5ccacb178f5f2ec52ec4
Foxylj <lijingy@usc.edu>
2022-05-10 15:59:45 -0700

    update

--- ARS1/ASR.py
+++ ARS1/ASR.py
@@ -12,7 +12,11 @@ from hmmlearn import hmm
 #from speech_features import mfcc
 import speech_recognition 
 from python_speech_features import mfcc
+from vad import VoiceActivityDetector
 from scipy import interpolate
+import matplotlib.pyplot as plt
+import librosa
+import librosa.display
 
 
 def on_click(x, y, button, pressed):
@@ -42,14 +46,11 @@ class ModelHMM(object):
 
         self.model = hmm.GaussianHMM(n_components=self.n_components, 
                 covariance_type=self.cov_type, n_iter=self.n_iter)
-
     # 'training_data' is a 2D numpy array where each row is 13-dimensional
     def train(self, training_data):
         np.seterr(all='ignore')
         cur_model = self.model.fit(training_data)
         self.models.append(cur_model)
-
-    # Run the HMM model for inference on input data
     def compute_score(self, input_data):
         return self.model.score(input_data)
     def save(self, path="models.pkl"):
@@ -61,7 +62,6 @@ class ModelHMM(object):
 def build_models(input_folder,input_way):
     # Initialize the variable to store all the models
     speech_models = []
-
     if input_way==1:
         for dirname in os.listdir(input_folder):
             # Get the name of the subfolder 
@@ -71,45 +71,31 @@ def build_models(input_folder,input_way):
 
             if not os.path.isdir(subfolder): 
                 continue
-
-            # Extract the label
             label = subfolder[subfolder.rfind('/') + 1:]
-            # Initialize the variables
             X = np.array([])
-
             # Create a list of files to be used for training
-            # We will leave one file per folder for testing
-            training_files = [x for x in os.listdir(subfolder) if x.endswith('.wav')][:-1]
-
+            training_files = [x for x in os.listdir(subfolder) if x.endswith('.wav')]
             # Iterate through the training files and build the models
             for filename in training_files: 
-                # Extract the current filepath
                 filepath = os.path.join(subfolder, filename)
-
-                # Read the audio signal from the input file
+                # Get freq and audio single
                 sampling_freq, signal = wavfile.read(filepath)
-                
                 # Extract the MFCC features
                 with warnings.catch_warnings():
                     warnings.simplefilter('ignore')
                     features_mfcc = mfcc(signal, sampling_freq)
-
                 # Append to the variable X
                 if len(X) == 0:
                     X = features_mfcc
                 else:
                     X = np.append(X, features_mfcc, axis=0)
-                
             # Create the HMM model
             model = ModelHMM()
-
             # Train the HMM
             model.train(X)
             model.save('Train/'+label+'.pkl')
             # Save the model for the current word
             speech_models.append((model, label))
-            print(model)
-            # Reset the variable
             model = None
         return speech_models
     else:
@@ -119,49 +105,59 @@ def build_models(input_folder,input_way):
             label = dirname
             print(label)
             model = ModelHMM()
-            #model.train(X)
             model.load('Train/'+label+'.pkl')
             print(model)
             # Save the model for the current word
             speech_models.append((model, label))
-
-            # Reset the variable
             model = None
         return speech_models
 
-# Define a function to run tests on input files
 def run_tests(test_files):
-    # Classify input data
     for test_file in test_files:
-        # Read input file
-        sampling_freq, signal = wavfile.read(test_file)
+        #sampling_freq, signal = wavfile.read(test_file)
+        v = VoiceActivityDetector(test_file)
+        sample_rate = v.rate
+        data_length = len(v.data)
+        raw_detection = v.detect_speech()
+        speech_labels = v.convert_windows_to_readible_labels(raw_detection)
+        # v.plot_detected_speech_regions()
+        #print(speech_labels)
+        list = []
+        for index in speech_labels: #VAD
+            x = index.get("speech_begin")
+            y = index.get("speech_end")
+            for i in range(int(sample_rate*x), int(sample_rate*y)):
+                list.append(v.data[i])
+        result = np.array(list)
+        result = result.astype(float)
         with warnings.catch_warnings():
             warnings.simplefilter('ignore')
-            features_mfcc = mfcc(signal, sampling_freq)
-
-        # Define variables
+            features_mfcc = mfcc(result, sample_rate)
+        '''print(features_mfcc)
+        y, sr = librosa.load(test_file)
+        mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40)
+        plt.figure(figsize=(10, 4))
+        librosa.display.specshow(mfccs, x_axis='time')
+        plt.colorbar()
+        plt.title('MFCC')
+        plt.tight_layout()'''
         max_score = -float('inf') 
         output_label = None 
-
-        # Run the current feature vector through all the HMM
-        # models and pick the one with the highest score
         for item in speech_models:
             model, label = item
-            score = model.compute_score(features_mfcc)
+            score = model.compute_score(features_mfcc) #where score is log probability
             if score > max_score:
                 max_score = score
                 predicted_label = label
-                print(predicted_label)
-
+                print(predicted_label,score)
         print('Predicted:', predicted_label)
 
 
 
 
 if __name__=='__main__':
-    #args = build_arg_parser().parse_args()
     input_wav = 'hmm-speech-recognition-0.1/audio'
-    # Build an HMM model for each word
+    # Build an HMM model for each number
     speech_models = build_models(input_wav,1)
 
     print("Ready!")
---
+++ ARS1/readme.txt
@@ -0,0 +1,12 @@
+Please install the software in requirements.txt before running the file.
+
+Then use python3 ASR.py to run the program
+
+If there are any programs not installed please use pip3 install #package# to install the software
+
+When the training is over, it will display "Ready!" and then click any button of the mouse to start recording
+
+After 3 seconds, the recording ends and the prediction starts.
+
+References：
+https://github.com/atta007/Speech-Recognizer/blob/main/speech_recognizer.ipynb
