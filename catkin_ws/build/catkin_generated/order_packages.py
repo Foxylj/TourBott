170f788f10f438969f0bab16901fed447e8cebcf
Amin Mahjoub <mahjoub@usc.edu>
2022-05-10 16:50:01 -0700

    Delete catkin_ws directory

--- catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/jetbot/catkin_ws/src'
-whitelisted_packages = 'localization_data_pub'.split(';') if 'localization_data_pub' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/jetbot/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/jetbot/catkin_ws/devel;/opt/ros/melodic' != '' else []
--- catkin_ws/build/jetson_nano_bot/localization_data_pub/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jetbot/catkin_ws/src/jetson_nano_bot/localization_data_pub
-# Build directory: /home/jetbot/catkin_ws/build/jetson_nano_bot/localization_data_pub
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
